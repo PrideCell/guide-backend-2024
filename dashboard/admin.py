5b85421e6639a410eed5802f03e4dbd7f846c25f
Leela Sri <leelasric@gmail.com>
2024-01-05 21:49:11 +0530

    list display in admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
 from .models import Log  
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', 'team_id', 'action')
+    ordering = ('timestamp',)
+    search_fields = ('team_id', 'action')
 
-admin.site.register(Log)
+admin.site.register(Log, LogAdmin)
+
 
